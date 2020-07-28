3c0d62d55d6a68cf979b502764ea9bc8a8990866
nozavroni <luke.visinoni@gmail>
2020-07-28 03:13:57 -0700

    Added ability to specify only certain type of exception should be swapped

--- src/Exception/Traits/Swappable.php
+++ src/Exception/Traits/Swappable.php
@@ -19,9 +19,14 @@ trait Swappable
      * Run a callable, catch any exception thrown and swap it out with the exception calling this method.
      *
      * @param callable $func A callable that might throw an exception to be swapped out
+     * @param string|null $type A class or interface to swap out for the calling class
      * @throws Throwable
+     * @todo Accept an argument that tells which kind(s) of exception to swap, for instance if you called
+     *       a function that throws a LogicException OR a RuntimeException but you only want
+     *       RuntimeException to be swapped for a DomainException you would call
+     *       DomainException::swap($func, RuntimeException::class)
      */
-    public static function swap(callable $func)
+    public static function swap(callable $func, ?string $type = null)
     {
         $class = static::class;
         TypeError::raiseUnless(
@@ -31,10 +36,14 @@ trait Swappable
         return recover (
             fn () => value_of($func).
             null,
-            function ($throwable) use ($class) {
+            function ($throwable) use ($class, $type) {
                 // kind of a hack...using the handler argument (which is supposed to be passive)
                 // to throw the desired exception
-                throw new $class($throwable->getMessage(), $throwable->getCode(), $throwable);
+                if (is_null($type) || is_subclass_of($throwable, $type)) {
+                    throw new $class($throwable->getMessage(), $throwable->getCode(), $throwable);
+                }
+                // if no swap can be done, rethrow the original
+                throw $throwable;
             }
         );
     }
